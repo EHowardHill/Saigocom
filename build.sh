18932ff4e2706ef4f6cb9bc8be6e4a13d95da697
Ethan Hill <ethan.hill1999@outlook.com>
2023-10-27 22:05:05 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+sh aports/scripts/mkimage.sh --tag edge \
+	--outdir ~/iso \
+	--arch x86_64 \
+	--repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
+	--extra_repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
+	--profile saigo
