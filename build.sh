50b9578ee8353af3a1ae818f1d4923a77d0b526a
Ethan Hill <ethan.hill1999@outlook.com>
2023-10-27 22:08:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ sh aports/scripts/mkimage.sh --tag edge \
 	--outdir ~/iso \
 	--arch x86_64 \
 	--repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
-	--extra_repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
+	--extra-repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
 	--profile saigo
